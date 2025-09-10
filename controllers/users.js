872c58e119dceedca5789e6dba996cc93f7fb357
khalidn1 <Khalidnur12@gmail.com>
2025-09-10 16:58:19 -0500

    update

--- controllers/users.js
+++ controllers/users.js
@@ -45,6 +45,10 @@ const createUser = (req, res) => {
 const login = (req, res) => {
   const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(BAD_REQUEST).send({ message: 'Email and password are required' });
+  }
+
   User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
