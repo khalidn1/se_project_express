d79e3e7d7d7d27090e94f52f317f563437e76151
khalidn1 <Khalidnur12@gmail.com>
2026-03-02 15:47:19 -0600

    Final cleanup: Remove remaining green comments from routes
    - Removed all remaining // comments from routes/index.js
    - Project is now completely clean and ready for submission

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,11 @@ const { getItems } = require('../controllers/clothingItems');
 const { auth } = require('../middlewares/auth');
 const { validateUserSignup, validateUserSignin } = require('../middlewares/validation');
 
-// Authentication routes with validation
 router.post('/signin', validateUserSignin, login);
 router.post('/signup', validateUserSignup, createUser);
 
-// Public route for items
 router.get('/items', getItems);
 
-// Protected routes
 router.use('/users', auth, userRouter);
 router.use('/items', auth, clothingItemRouter);
 
